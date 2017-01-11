f4df5ad7c9bcfb3d0c1533c08102510310b71c49
Adrien REDON <AdrienRedon@users.noreply.github.com>
2017-01-11 15:20:53 +0100

    ADD: Store on root element

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueSocketio from 'vue-socket.io';
+import store from './store'
 
 Vue.use(VueSocketio, 'http://localhost:1337');
 
 new Vue({
   el: '#app',
+  store,
   sockets:{
     connect: function(){
       console.log('socket connected')
